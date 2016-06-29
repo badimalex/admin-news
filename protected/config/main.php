b8474ce9794cf4b3df818effe8bcb1985bded057
Badichan Dmitriy <badimalex@gmail.com>
2016-06-29 17:31:22 +0300

    User can create news

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,13 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'123',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
+		'admin',
 	),
 
 	// application components
