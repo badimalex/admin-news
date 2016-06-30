4ad8962552c10d0f53d309999402025bc35c684a
Badichan Dmitriy <badimalex@gmail.com>
2016-06-30 19:27:50 +0300

    heroku config

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,10 +1,22 @@
 <?php
-
+if ($_SERVER['SERVER_NAME'] == "frozen-fjord-31334.herokuapp.com") {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $dbname = substr($url["path"], 1);
+    $config = array(
+        'connectionString' => "mysql:host={$url["host"]};dbname={$dbname}",
+        'emulatePrepare' => true,
+        'username' =>  $url["user"],
+        'password' =>  $url["pass"],
+        'charset' =>'utf8',
+    );
+} else {
 // This is the database connection configuration.
-return array(
-	'connectionString' => 'mysql:host=localhost;dbname=news',
-	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '123',
-	'charset' => 'utf8',
-);
+    $config = array(
+        'connectionString' => 'mysql:host=localhost;dbname=news',
+        'emulatePrepare' => true,
+        'username' => 'root',
+        'password' => '123',
+        'charset' => 'utf8',
+    );
+}
+return $config;
